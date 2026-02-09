38b133d6e08a91eb938af14f9efd2537fb46fb7b
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-09 09:22:01 +0530

    Time: 6 ms (63.72%) | Memory: 12.4 MB (92.48%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s):
+        last = {}
+        for i, c in enumerate(s):
+            last[c] = i
+
+        res = []
+        start = end = 0
+
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
+
+        return res
