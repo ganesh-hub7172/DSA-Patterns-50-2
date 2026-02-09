f0ede67952c2906d591a1247144733994928d025
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-09 09:21:31 +0530

    Time: 100 ms (18.75%) | Memory: 13.1 MB (82.64%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums):
+        ops = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                needed = nums[i - 1] + 1
+                ops += needed - nums[i]
+                nums[i] = needed
+        return ops
